7bd034bb1ef63a2c2b5c8f316f108a9e65332409
CHOI, Hong Joon <hongjoonchoi95@gmail.com>
2021-01-13 02:03:25 +0900

    Update run_automated_test.sh

--- run_automated_test.sh
+++ run_automated_test.sh
@@ -4,11 +4,11 @@ python cut_image.py testimg_1.jpg 3 3 test1 -v # error
 python cut_image.py test_images/testimg_1.png 3 3 test1 -v #error
 
 python cut_image.py test_images/testimg_2 2 2 test1 -v # square image
-python cut_image.py test_images/testimg_1.jpg 3 3 test2 #standard
-python cut_image.py test_images/testimg_3 2 4 test3 #standard
-python cut_image.py test_images/testimg_1 4 1 test4 # edge case
-python cut_image.py test_images/testimg_4 12 14 test5 # computation heavy 1
-python cut_image.py test_images/testimg_1 7 7 test6 # square image computation heavy 2
+python cut_image.py test_images/testimg_1.jpg 3 3 test2 -v #standard
+python cut_image.py test_images/testimg_3 2 4 test3 -v #standard
+python cut_image.py test_images/testimg_1 4 1 test4 -v # edge case
+python cut_image.py test_images/testimg_4 12 14 test5 -v # computation heavy 1
+python cut_image.py test_images/testimg_1 7 7 test6 -v # computation heavy 2
 
 python merge_image.py test1 2 2 out1 -v
 python merge_image.py test2 3 3 out2 -v
