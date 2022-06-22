cbf23b2b6efef4ac48541d4a8ae3e664dfe0e95f
hjchoi95 <hongjoonchoi95@gmail.com>
2022-06-22 15:32:48 +0900

    feat: added minimum spanning tree animation
    closes #12

--- jigsaw_puzzle_solver/assembler/assembler_visualizer.py
+++ jigsaw_puzzle_solver/assembler/assembler_visualizer.py
@@ -9,7 +9,7 @@ import numpy as np
 
 MAX_PIECES_WINDOW_SIZE = (300, 400)  # height, width
 MAX_MERGE_ANIMATION_WINDOW_SIZE = (600, 1080)  # height, width
-
+ANIMATE_MINIMUM_SPANNING_TREE = False
 
 def start_assembly_animation(merge_history, img_cells_unaligned, img_cells, interval_millis=200):
     """
@@ -25,17 +25,23 @@ def start_assembly_animation(merge_history, img_cells_unaligned, img_cells, inte
 
     if not merge_history:
         return
-
     rows, cols = merge_history[-1]["cellblock"].block_size()
-    for i, _ in enumerate(merge_history):
-        _display_current_assembly_progress(merge_history[i], merged_id_list, img_cells_unaligned,
-                                           img_cells, rows, cols,
-                                           str(i + 1) + "/" + str(len(merge_history)),
-                                           True, interval_millis)
-    _display_current_assembly_progress(merge_history[-1], merged_id_list, img_cells_unaligned,
-                                       img_cells, rows, cols,
-                                       str(len(merge_history)) + "/" + str(len(merge_history)),
-                                       False, interval_millis * 4)
+
+    if ANIMATE_MINIMUM_SPANNING_TREE:
+        for i, _ in enumerate(merge_history):
+            _display_minimum_spanning_tree(merge_history[i], img_cells, rows, cols, interval_millis)
+        _display_minimum_spanning_tree(merge_history[-1], img_cells, rows, cols, interval_millis)
+    else:
+        for i, _ in enumerate(merge_history):
+            _display_current_assembly_progress(merge_history[i], merged_id_list,
+                                               img_cells_unaligned, img_cells, rows, cols,
+                                               str(i + 1) + "/" + str(len(merge_history)),
+                                               True, interval_millis)
+        _display_current_assembly_progress(merge_history[-1], merged_id_list,
+                                           img_cells_unaligned, img_cells, rows, cols,
+                                           str(len(merge_history)) + "/" + str(len(merge_history)),
+                                           False, interval_millis * 4)
+
     cv2.destroyAllWindows()
 
 
@@ -142,5 +148,67 @@ def _display_current_assembly_progress(merge, merged_id_list, img_cells_unaligne
         _display_remaining_pieces(img_cells_unaligned, merged_id_list, rows, cols, celldata.img_id)
     except ValueError:
         pass
+    cv2.imshow("jigsaw puzzle solver", whiteboard)
+    cv2.waitKey(interval_millis)
+
+
+def _display_minimum_spanning_tree(merge, img_cells, rows, cols, interval_millis):
+    """
+    Private method, shows remaining image fragments.
+    """
+    cellblock = merge["cellblock"].data
+    bottom, right, top, left = merge["cellblock"].top, merge["cellblock"].right, \
+                               merge["cellblock"].bottom, merge["cellblock"].left
+
+    max_h = MAX_MERGE_ANIMATION_WINDOW_SIZE[0]
+    max_w = MAX_MERGE_ANIMATION_WINDOW_SIZE[1]
+    cell_h = len(img_cells[0][0])
+    cell_w = len(img_cells[0][0][0])
+    window_h = rows * cell_h + int(cell_h * 1.0)
+    window_w = cols * cell_w + int(cell_w * 1.0)
+    cellblock_h = (bottom - top + 1) * cell_h
+    cellblock_w = (right - left + 1) * cell_w
+    start_h = (window_h - cellblock_h) // 2
+    start_w = (window_w - cellblock_w) // 2
+    scale = min(max_h / window_h, max_w / window_w)
+
+    whiteboard = np.zeros((window_h, window_w, 3), dtype=np.uint8)
+    whiteboard.fill(255)
+
+    # assemble image as specifed in cellblock blueprint
+    for i, _ in enumerate(cellblock):
+        for j, _ in enumerate(cellblock[0]):
+            if cellblock[i][j].is_valid():
+                cdata = cellblock[i][j]
+                paste = img_cells[cdata.img_id][cdata.transform]
+                y_offset = start_h + (i - top) * cell_h
+                x_offset = start_w + (j - left) * cell_w
+                whiteboard[y_offset:y_offset + cell_h, x_offset:x_offset + cell_w] = paste
+
+    for i, _ in enumerate(cellblock):
+        for j, _ in enumerate(cellblock[0]):
+            if cellblock[i][j].is_valid():
+                cdata = cellblock[i][j]
+                y_offset = start_h + (i - top) * cell_h
+                x_offset = start_w + (j - left) * cell_w
+                if cdata.dir == 0:
+                    cv2.line(whiteboard, (x_offset + cell_w // 2, y_offset + cell_h // 2),
+                             (x_offset + cell_w // 2, y_offset + 3 * cell_h // 2),
+                             (0, 0, 255), int(2 / scale))
+                if cdata.dir == 1:
+                    cv2.line(whiteboard, (x_offset + cell_w // 2, y_offset + cell_h // 2),
+                             (x_offset + cell_w // 2, y_offset - cell_h // 2),
+                             (0, 0, 255), int(2 / scale))
+                if cdata.dir == 2:
+                    cv2.line(whiteboard, (x_offset + cell_w // 2, y_offset + cell_h // 2),
+                             (x_offset + 3 * cell_w // 2, y_offset + cell_h // 2),
+                             (0, 0, 255), int(2 / scale))
+                if cdata.dir == 3:
+                    cv2.line(whiteboard, (x_offset + cell_w // 2, y_offset + cell_h // 2),
+                             (x_offset - cell_w // 2, y_offset + cell_h // 2),
+                             (0, 0, 255), int(2 / scale))
+
+    whiteboard = cv2.resize(whiteboard, (int(window_w * scale), int(window_h * scale)))
+    whiteboard = cv2.copyMakeBorder(whiteboard, 0, 100, 0, 0, cv2.BORDER_CONSTANT)
     cv2.imshow("jigsaw puzzle solver", whiteboard)
     cv2.waitKey(interval_millis)
