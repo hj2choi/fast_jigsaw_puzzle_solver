614adab6916ebc05d573ee893327ffe448387dff
hjchoi95 <hongjoonchoi95@gmail.com>
2022-12-14 18:29:37 +0900

    chore: modify main() to take in specific values instead of as args and cfg objects.
    this is done to make integrated testing easier

--- jigsaw_puzzle_solver/assemble_images.py
+++ jigsaw_puzzle_solver/assemble_images.py
@@ -28,21 +28,16 @@ DEFAULT_CONFIG = {
 }
 
 
-def main(args, cfg):
+def main(imgs_prefix, cols, rows,
+         imgs_dir="image_fragments", out_dir="images_out", verbose=False,
+         show_anim=True, anim_interval=200, show_mst_on_anim=False):
     """
-    main jigsaw puzzle solver routine
+    main jigsaw puzzle solver routine.
     """
     s_time = time.time()
-    images_dir = cfg.get("config", "fragments_dir")
-    output_directory = cfg.get("config", "output_dir")
-    show_assembly_animation = cfg.getboolean("config", "show_assembly_animation") or \
-        args.show_animation
-    verbose = cfg.getboolean("config", "debug") or args.verbose
-    animation_interval = int(cfg.get("config", "animation_interval_millis"))
-
     # initialize
     assembler = asm.ImageAssembler.load_from_filepath(
-        images_dir, args.in_prefix, args.cols, args.rows)
+        imgs_dir, imgs_prefix, cols, rows)
     print("assemble_image.py:", len(assembler.raw_imgs), "files loaded", flush=True)
     if assembler.max_rows * assembler.max_cols != len(assembler.raw_imgs):
         print("WARNING: incorrect slicing dimension.")
@@ -53,33 +48,36 @@ def main(args, cfg):
     assembler.assemble()
 
     # save result to output directory, and show animation.
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-    assembler.save_assembled_image(output_directory + "/" + args.out_filename)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+    assembler.save_assembled_image(out_dir + "/" + imgs_prefix)
     sys.stdout = sys.__stdout__  # restore stdout
     print("total elapsed time:", time.time() - s_time, "seconds", flush=True)
-    if show_assembly_animation:
-        assembler.start_assembly_animation(args.show_spanning_tree, animation_interval)
+    if show_anim:
+        assembler.start_assembly_animation(show_mst_on_anim, anim_interval)
 
 
 if __name__ == '__main__':
-    AP = ArgumentParser()
-    AP.add_argument('in_prefix', type=str, help='prefix to image fragments')
-    AP.add_argument('cols', type=int, help='Number of column slices')
-    AP.add_argument('rows', type=int, help='Number of row slices')
-    AP.add_argument('out_filename', type=str, help='filename for reconstructed image')
-    AP.add_argument('--verbose', '-v', required=False, action='store_true',
+    ap = ArgumentParser()
+    ap.add_argument('img_prefix', type=str, help='prefix to image fragments')
+    ap.add_argument('cols', type=int, help='Number of column slices')
+    ap.add_argument('rows', type=int, help='Number of row slices')
+    ap.add_argument('--verbose', '-v', required=False, action='store_true',
                     help='increase output verbosity')
-    AP.add_argument('--show_animation', '-a', required=False, action='store_true',
+    ap.add_argument('--show_animation', '-a', required=False, action='store_true',
                     help='show image reconstruction animation')
-    AP.add_argument('--show_spanning_tree', '-t', required=False, action='store_true',
+    ap.add_argument('--show_spanning_tree', '-t', required=False, action='store_true',
                     help='show minimum spanning tree on top of the animation (-a option requried)')
-    AP.add_argument('--config_file', '-c', required=False, default="./config/config.ini",
+    ap.add_argument('--config_file', '-c', required=False, default="./config/config.ini",
                     action='store', nargs=1, help='configuration ini file')
-    PARSED_ARGS = AP.parse_args()
+    args = ap.parse_args()
 
-    CP = ConfigParser()
-    CP.read_dict(DEFAULT_CONFIG)
-    CP.read(PARSED_ARGS.config_file)
+    cp = ConfigParser()
+    cp.read_dict(DEFAULT_CONFIG)
+    cp.read(args.config_file)
 
-    main(PARSED_ARGS, CP)
+    main(args.img_prefix, args.cols, args.rows, cp.get("config", "fragments_dir"), cp.get("config", "output_dir"),
+         cp.getboolean("config", "debug") or args.verbose,
+         cp.getboolean("config", "show_assembly_animation") or args.show_animation,
+         int(cp.get("config", "animation_interval_millis")), args.show_spanning_tree
+         )
--- jigsaw_puzzle_solver/fragment_image.py
+++ jigsaw_puzzle_solver/fragment_image.py
@@ -76,7 +76,8 @@ def process_image_segment(img, file_prefix, sequence_no):
     print("")
 
 
-def main(args, cfg):
+def main(img_path, col_slice, row_slice, fragments_prefix,
+         verbose=False, fragments_dir="image_fragments"):
     """
     1. read and slice image into y*x pieces as specified in args
     2. for each image slice: apply random set of transformations.
@@ -85,55 +86,52 @@ def main(args, cfg):
     s_time = time.time()
     random.seed(RANDOM_SEED)  # for reproducibility
 
-    output_dir = cfg.get("config", "fragments_dir")
-    verbose = cfg.getboolean("config", "debug") or args.verbose
-
     # read image
-    img = cv2.imread(args.image)
+    img = cv2.imread(img_path)
     if img is None:
         print("ERROR: cannot read file")
         return
-    print("fragment_image.py: loaded image from", args.image)
+    print("fragment_image.py: loaded image from", img_path)
 
     if not verbose:
         sys.stdout = open(os.devnull, 'w')  # block stdout
 
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
+    if not os.path.exists(fragments_dir):
+        os.makedirs(fragments_dir)
     # remove all image fragment files sharing the same designated prefix.
-    for file in filter(lambda fname: fname.startswith(args.out_prefix) and
-                       fname.endswith(".png"), os.listdir(output_dir)):
-        os.remove(os.path.join(output_dir, file))
+    for file in filter(lambda fname: fname.startswith(fragments_prefix) and
+                       fname.endswith(".png"), os.listdir(fragments_dir)):
+        os.remove(os.path.join(fragments_dir, file))
 
     # slice image into uniform shapes and process each slices
-    h, w = len(img) // args.rows, len(img[0]) // args.cols  # height and width
-    for i in range(args.rows):
-        for j in range(args.cols):
+    h, w = len(img) // row_slice, len(img[0]) // col_slice  # height and width
+    for i in range(row_slice):
+        for j in range(col_slice):
             top, left = int(i * h), int(j * w)  # row start, col start
             process_image_segment(img[top: top + h, left: left + w],
-                                  os.path.join(output_dir, args.out_prefix),
-                                  i*args.cols+j)
+                                  os.path.join(fragments_dir, fragments_prefix),
+                                  i*col_slice+j)
 
     sys.stdout = sys.__stdout__  # restore stdout
-    print("fragmented image into", args.rows * args.cols, "slices. ")
+    print("fragmented image into", row_slice * col_slice, "slices. ")
     print(time.time() - s_time, "seconds elapsed")
 
 
 if __name__ == '__main__':
-    AP = ArgumentParser()
-    AP.add_argument('image', type=str, help='Path to input image')
-    AP.add_argument('cols', type=int, help='Number of column slices')
-    AP.add_argument('rows', type=int, help='Number of row slices')
-    AP.add_argument('out_prefix', type=str, help='filename prefix to fragmented image files')
-    AP.add_argument('--verbose', '-v', required=False, action='store_true',
+    ap = ArgumentParser()
+    ap.add_argument('img_path', type=str, help='Path to an input image')
+    ap.add_argument('col_slice', type=int, help='Number of column slices')
+    ap.add_argument('row_slice', type=int, help='Number of row slices')
+    ap.add_argument('fragments_prefix', type=str, help='filename prefix to fragmented image files')
+    ap.add_argument('--verbose', '-v', required=False, action='store_true',
                     help='increase output verbosity')
-    AP.add_argument('--config_file', '-c', required=False, default="./config/config.ini",
+    ap.add_argument('--config_file', '-c', required=False, default="./config/config.ini",
                     action='store', nargs=1, help='configuration ini file')
-    PARSED_ARGS = AP.parse_args()
+    args = ap.parse_args()
 
-    CP = ConfigParser()
-    CP.read_dict(DEFAULT_CONFIG)
-    CP.read(PARSED_ARGS.config_file)
+    cp = ConfigParser()
+    cp.read_dict(DEFAULT_CONFIG)
+    cp.read(args.config_file)
 
-    main(PARSED_ARGS, CP)
+    main(args.img_path, args.col_slice, args.row_slice, args.fragments_prefix,
+         cp.getboolean("config", "debug") or args.verbose, cp.get("config", "fragments_dir"))
